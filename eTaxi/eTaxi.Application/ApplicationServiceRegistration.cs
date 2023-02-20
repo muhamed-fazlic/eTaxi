a66b488c3cafb79b062653ee317b35ec7bdcdd38
muhamed-fazlic <m.fazlic4@gmail.com>
2023-02-20 03:11:22 +0100

    Added AutoMapper and MediatR

---
+++ eTaxi/eTaxi.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace eTaxi.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
