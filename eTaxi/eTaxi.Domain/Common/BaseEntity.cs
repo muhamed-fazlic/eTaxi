d7c344cb7570b1c5c8f32572d1cf1dbdc8b87b8c
muhamed-fazlic <m.fazlic4@gmail.com>
2023-02-20 02:30:54 +0100

    Added domain project and created base entity

---
+++ eTaxi/eTaxi.Domain/Common/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace eTaxi.Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime? DateCreated { get; set; }
+        public DateTime? DateModified { get; set; }
+    }
+}
